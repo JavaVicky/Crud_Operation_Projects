17aaad43d9117f1d2c8b9b48ccafce47f548f9be
JavaVicky <bbnayak2019@gmail.com>
2021-09-02 21:56:29 +0530

    added all file

---
+++ src/main/java/com/byndr/project/SpringbootCrudPaginationSortingApplication.java
@@ -0,0 +1,13 @@
+package com.byndr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudPaginationSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudPaginationSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byndr/project/SpringbootCrudPaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byndr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudPaginationSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
